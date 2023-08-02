2f4a99cd7ce57b5880783cb81e637f9d2efc5347
Valentina Rogojina <rogojinavaliusha@gmail.com>
2023-08-02 17:17:55 +0300

    add Exemplu3

---
+++ Exemplu2/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Console.WriteLine("Introduceti Numele:");
+string username  = Console.ReadLine();
+Console.WriteLine("Salut!");
+Console.WriteLine(username);
+*/
+
+Console.Write("Introduceti Numele:");
+string username  = Console.ReadLine();
+Console.Write("Salut!");
+Console.Write(username);
+
