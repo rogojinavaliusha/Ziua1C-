2acf2c759f5c3b85a3942b30ac0915f9ac9437b9
Valentina Rogojina <rogojinavaliusha@gmail.com>
2023-08-02 18:00:29 +0300

    Exeplu nou

---
+++ Exemplu5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Scrie Numele: ");
+string username = Console.ReadLine();
+
+if(username == "Maria")
+{
+    Console.WriteLine("Uraaa asta e Maria!");
+}
+else
+{
+    Console.Write("Salut, ");
+    Console.WriteLine(username);
+}
+
+
+
